714752c886c12dbccd3b0ca0d503939699bd4633
Arman Mursali <manmursali17@gmail.com>
2026-09-22 17:55:46 +0800

    update img head

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -30,9 +30,8 @@
             }
         </style>
 
-        <link rel="icon" href="/favicon.ico" sizes="any">
-        <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="icon" href="{{ asset('img/logo.png') }}" type="image/png">
+        <link rel="apple-touch-icon" href="{{ asset('img/logo.png') }}">
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer">
 
